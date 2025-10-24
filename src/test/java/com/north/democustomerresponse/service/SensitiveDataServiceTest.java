2c8ba50e771282735a64145c40a8497d154b0359
Menkung <wadcharapong.men@gmail.com>
2025-10-24 14:47:43 +0700

    optimize import

--- src/test/java/com/north/democustomerresponse/service/SensitiveDataServiceTest.java
+++ src/test/java/com/north/democustomerresponse/service/SensitiveDataServiceTest.java
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.stereotype.Service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
 class SensitiveDataServiceTest {
